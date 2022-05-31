f43002fc93aece324d15a1e6f1c845ef6a392ac4
Junkerus <guisove@gmail.com>
2022-05-31 14:51:08 -0400

    coursera

---
+++ javascript/javascript.js
@@ -0,0 +1,47 @@
+/*alert('estamos en javascript'); /*alerta muestra un mensaje*/
+/*prompt("para ingresar datos"); /*muestra una ventana que solicita ingresar datos*/
+/* funciones para convertir datos para
+String(), Number(), Boolean(), Symbol(), BigInt()*/
+
+/*funciones especiales para cambiar de un tipo a otros
+parseInt(), parseFloat(), parseString(), parseBoolean(), parseSymbol(), parseBigInt()*/
+
+/*coercion*/
+/*tipo de coercion:
+1. implicita, ejemplo: var a = 10; var b = a + "10"; b es un string porque se hace la conversion implicita,
+
+2. explicita, ejemplo: var a = 10; var b = String(a); b es un string porque se hace la conversion explicita
+
+3. no hay coercion*/
+
+/*ciclos / condiciones*/
+/*intruccion inicial; condicion; incremento*/
+/*while, es un ciclo que se repite mientras la condicion sea verdadera*/
+/*do while, es un ciclo que se repite al menos una vez, y luego se evalua la condicion*/
+/*for, es un ciclo que se repite un numero de veces, y luego se evalua la condicion*/
+/*for in, solo itera sobre propiedades enumerables que no son símbolo*/
+/*for of, ejecuta un bloque de código para cada elemento de un objeto iterable, como lo son: String, Array*/
+/*forEach, es una funcion indicada una vez por cada elemento del array.
+/*forEachIn, es una funcion que recorre un array*/
+/*forEachOf, es una funcion que recorre un array y ejecuta una funcion*/
+
+/*operadores de comparacion*/
+/*==, igualdad, compara si dos valores son iguales*/
+/*===, igualdad estricta, compara si dos valores son iguales y tienen el mismo tipo*/
+/*!=, desigualdad, compara si dos valores son diferentes*/
+/*!==, desigualdad estricta, compara si dos valores son diferentes y tienen el mismo tipo*/
+/*<, menor que, compara si un valor es menor que otro*/
+/*<=, menor o igual que, compara si un valor es menor o igual que otro*/
+/*>, mayor que, compara si un valor es mayor que otro*/
+/*>=, mayor o igual que, compara si un valor es mayor o igual que otro*/
+
+/*operadores logicos*/
+/*&&, and, compara si dos valores son verdaderos*/
+/*||, or, compara si dos valores son verdaderos*/
+/*! not, compara si un valor es falso*/
+/*??, funcion de nulos*/
+
+/*condiciones*/
+/*if, es una condicion que se evalua si una condicion es verdadera*/
+/*if else, es una condicion que se evalua si una condicion es verdadera, y si no, se evalua otra condicion*/
+/*switch, evalúa una expresión, comparando el valor de esa expresión con una instancia*/
